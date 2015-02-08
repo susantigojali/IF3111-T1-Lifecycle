1f609f855a664664ba6951a6ee452324d37787db
susantigojali <susantigojali@hotmail.com>
2015-02-08 11:28:03 +0700

    Exercise3: Receiver Phone

---
+++ ReceiverPhone/app/src/main/java/phone/receiver/android/sg/com/receiverphone/MyPhoneReceiver.java
@@ -0,0 +1,29 @@
+package phone.receiver.android.sg.com.receiverphone;
+
+/**
+ * Created by susanti_2 on 07-Feb-15.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class MyPhoneReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            String state = extras.getString(TelephonyManager.EXTRA_STATE);
+            Log.w("MY_DEBUG_TAG", state);
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                String phoneNumber = extras
+                        .getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                Log.w("MY_DEBUG_TAG", phoneNumber);
+            }
+        }
+    }
+
+}
